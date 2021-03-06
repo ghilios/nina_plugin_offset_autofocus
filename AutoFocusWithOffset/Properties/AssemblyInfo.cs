fa5bf7a6232c3da0082c7950e65949a63646db4f
George Hilios <ghilios@gmail.com>
2021-03-05 20:06:54 -0500

    Initial commit for plugin template

---
+++ AutoFocusWithOffset/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NINA.Plugin.AutoFocusWithOffset")]
+[assembly: AssemblyDescription("NINA Trigger plugins for AutoFocus with a configurable focuser offset")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4400d156-486b-4fb1-b5b8-483da29a3d9c")]
